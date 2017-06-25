d5daf1983e1120a54540bce5c2cf5d6379e80433
Chung Nguyễn <chung@Mac-Pro.local>
2017-06-26 06:29:24 +0700

    Initial

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const request = require('request');
+const async = require('async');
+
+import mongoose from 'mongoose'
+import Shows from './models/shows'
+
+const app = express();
+const numTvShows = 1000
+
+mongoose.connect('mongodb://localhost/shows');
+
+//fetch tv shows from tvmaze and save into mongoDB
+app.get('/all', (req, res, next) => {
+    async.timesLimit(numTvShows, 2, (i, callback) => {
+        setTimeout(function(){
+            var options = {
+                url: 'http://api.tvmaze.com/shows/'+(i+1)+"?embed[]=episodes&embed[]=cast",
+            }
+            request(options, (error, response, body) => {
+                var result = JSON.parse(body)
+                var data = {
+                    id: result.id ,
+                    name: result.name ,
+                    image: result.image ? result.image.original : null ,
+                    details: {
+                        genres: result.genres ,
+                        year: result.premiered ,
+                        description: result.summary ,
+                        cast: result._embedded ? result._embedded.cast : null ,
+                        episodes: result._embedded ? result._embedded.episodes : null
+                    }
+                };
+                console.log(i);
+                callback(null, data);
+            });
+            }, 1000)
+            }, (err, results) => {
+                results.map((items) => {
+                    const show = new Shows(items)
+                    show.save()
+                })
+        });
+         return next()
+})
+
+//get all shows that id is equal to genre 
+app.get('/shows/:id', (req, res, next) => {
+    Shows.find({'details.genres': {$in: [req.params.id]}},function(err, shows){
+        res.json(shows)
+    })
+})
+
+app.listen('3000', () => {
+    console.log('Listening on port 3000');
+})
